a4f3b7dbea09c0915ff3e2caa9c23e25e1fe2695
ashleysyheo <ashleysyheo@gmail.com>
2023-07-06 20:39:57 +0900

    feat: Spinner 컴포넌트 생성
    Co-authored-by: 임우찬 <dlaxodud1217@gmail.com>
    Co-authored-by: Dahye Yun <Dahyeeee@users.noreply.github.com>

---
+++ src/styles/animation/index.ts
@@ -0,0 +1,10 @@
+import { keyframes } from '@emotion/react';
+
+export const spinnerRotation = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
