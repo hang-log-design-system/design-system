ee4f0f52eb707c7d53c286f8528a61e4e01cec19
ashleysyheo <ashleysyheo@gmail.com>
2023-07-07 09:44:26 +0900

    feat: Input 컴포넌트 생성

---
+++ src/components/Input/Input.style.ts
@@ -0,0 +1,90 @@
+import { Theme } from '@/styles/Theme';
+import { css } from '@emotion/react';
+
+import type { InputProps } from '@components/Input/Input';
+
+export const inputContainerStyling = css({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: Theme.spacer.spacing2,
+});
+
+export const inputWrapperStyling = (isError: Required<InputProps>['isError']) => {
+  return css({
+    display: 'flex',
+    gap: '12px',
+    alignItems: 'center',
+
+    paddingTop: 0,
+    paddingBottom: 0,
+    borderRadius: Theme.borderRadius.small,
+
+    backgroundColor: isError ? `${Theme.color.red100} !important` : 'transparent',
+    boxShadow: isError ? `0 0 0 1px ${Theme.color.red200}` : 'none',
+
+    '&:focus-within': {
+      boxShadow: isError
+        ? `0 0 0 1px ${Theme.color.red200}, 0 0 0 3px ${Theme.color.red100}`
+        : `0 0 0 1px ${Theme.color.gray300}, 0 0 0 3px ${Theme.color.gray100}`,
+    },
+
+    '& svg': {
+      width: '16px',
+      height: '16px',
+    },
+  });
+};
+
+export const getVariantStyling = (variant: Required<InputProps>['variant']) => {
+  const style = {
+    default: css({
+      backgroundColor: Theme.color.gray100,
+    }),
+
+    text: css({
+      backgroundColor: 'transparent',
+    }),
+  };
+
+  return style[variant];
+};
+
+export const getSizeStyling = (size: Required<InputProps>['size']) => {
+  const style = {
+    large: css({
+      padding: '14px 16px',
+
+      fontSize: Theme.text.medium.fontSize,
+      lineHeight: Theme.text.medium.lineHeight,
+    }),
+
+    medium: css({
+      padding: '12px 16px',
+
+      fontSize: Theme.text.medium.fontSize,
+      lineHeight: Theme.text.medium.lineHeight,
+    }),
+
+    small: css({
+      padding: '8px 12px',
+
+      fontSize: Theme.text.small.fontSize,
+      lineHeight: Theme.text.small.lineHeight,
+    }),
+  };
+
+  return style[size];
+};
+
+export const getInputStyling = (isError: Required<InputProps>['isError']) => {
+  return css({
+    width: '100%',
+    paddingLeft: 0,
+    paddingRight: 0,
+    border: 'none',
+    borderRadius: Theme.borderRadius.small,
+    outline: 0,
+
+    backgroundColor: 'transparent',
+  });
+};
