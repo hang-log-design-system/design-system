c761ad1e7c586e2764326907623a5c60dc690dfa
ashleysyheo <ashleysyheo@gmail.com>
2023-07-07 09:45:11 +0900

    feat: Textarea 컴포넌트 생성

---
+++ src/components/Textarea/Textarea.style.ts
@@ -0,0 +1,56 @@
+import { Theme } from '@/styles/Theme';
+import { css } from '@emotion/react';
+
+import type { TextareaProps } from '@components/Textarea/Textarea';
+
+export const textareaContainerStyling = css({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: Theme.spacer.spacing2,
+});
+
+export const getSizeStyling = (size: Required<TextareaProps>['size']) => {
+  const style = {
+    large: css({
+      padding: '14px 16px',
+
+      fontSize: Theme.text.medium.fontSize,
+      lineHeight: Theme.text.medium.lineHeight,
+    }),
+
+    medium: css({
+      padding: '12px 16px',
+
+      fontSize: Theme.text.medium.fontSize,
+      lineHeight: Theme.text.medium.lineHeight,
+    }),
+
+    small: css({
+      padding: '8px 12px',
+
+      fontSize: Theme.text.small.fontSize,
+      lineHeight: Theme.text.small.lineHeight,
+    }),
+  };
+
+  return style[size];
+};
+
+export const getTextareaStyling = (isError: Required<TextareaProps>['isError']) => {
+  return css({
+    width: '100%',
+    padding: 0,
+    border: 'none',
+    borderRadius: Theme.borderRadius.small,
+    outline: 0,
+
+    backgroundColor: isError ? `${Theme.color.red100} !important` : Theme.color.gray100,
+    boxShadow: isError ? `0 0 0 1px ${Theme.color.red200}` : 'none',
+
+    '&:focus-within': {
+      boxShadow: isError
+        ? `0 0 0 1px ${Theme.color.red200}, 0 0 0 3px ${Theme.color.red100}`
+        : `0 0 0 1px ${Theme.color.gray300}, 0 0 0 3px ${Theme.color.gray100}`,
+    },
+  });
+};
