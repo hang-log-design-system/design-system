d98bb3bdf26dc4b1963acf29a43f1ca4df5bb090
ashleysyheo <ashleysyheo@gmail.com>
2023-07-08 19:57:02 +0900

    chore: Select 컴포넌트 jsdoc 주석 추가

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -8,10 +8,23 @@ import { getSizeStyling } from '@components/Select/Select.style';
 import SupportingText from '@components/SupportingText/SupportingText';
 
 export interface SelectProps extends Omit<ComponentPropsWithRef<'select'>, 'size'> {
+  /** Select의 라벨 텍스트 */
   label?: string;
+  /**
+   * Select의 시이즈
+   *
+   * @default 'medium'
+   */
   size?: Extract<Size, 'small' | 'medium' | 'large'>;
+  /**
+   * Select 인풋의 에러 여부
+   *
+   * @default false
+   */
   isError?: boolean;
+  /** Select에서 선택할 수 있는 JSX option 요소들 */
   children: JSX.Element | JSX.Element[];
+  /** Select의 부가 정보 텍스트 */
   supportingText?: string;
 }
 
