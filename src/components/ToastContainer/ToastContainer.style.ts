0aeda12229f82a3e4129650cc1cb7c9b30648ff0
ashleysyheo <ashleysyheo@gmail.com>
2023-07-10 15:16:14 +0900

    feat: ToastContainer 컴포넌트 생성

---
+++ src/components/ToastContainer/ToastContainer.style.ts
@@ -0,0 +1,13 @@
+import { css } from '@emotion/react';
+
+export const containerStyling = css({
+  position: 'absolute',
+  bottom: '24px',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '16px',
+  justifyContent: 'center',
+  alignItems: 'center',
+
+  width: '100%',
+});
