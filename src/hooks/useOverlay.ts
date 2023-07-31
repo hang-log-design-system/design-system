1f4977092e2bbfdef0798cad66c2642f1aa1b98f
Dahye <06robin11@gmail.com>
2023-07-31 15:11:24 +0900

    refactor: useOverlay usecallback 적용

--- src/hooks/useOverlay.ts
+++ src/hooks/useOverlay.ts
@@ -1,19 +1,19 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 export const useOverlay = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const open = () => {
+  const open = useCallback(() => {
     setIsOpen(true);
-  };
+  }, []);
 
-  const close = () => {
-    setIsOpen(false);
-  };
+  const close = useCallback(() => {
+    setIsOpen(true);
+  }, []);
 
-  const toggle = () => {
+  const toggle = useCallback(() => {
     setIsOpen((prev) => !prev);
-  };
+  }, []);
 
   return { isOpen, open, close, toggle };
 };
