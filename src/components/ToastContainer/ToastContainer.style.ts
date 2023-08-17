bc9d0ad7670750824f0bf916ffa46f6c94f6f7af
Ashley Heo <ashleysyheo@gmail.com>
2023-08-17 15:21:26 +0900

    refactor: Toast 수정

--- src/components/ToastContainer/ToastContainer.style.ts
+++ src/components/ToastContainer/ToastContainer.style.ts
@@ -1,7 +1,7 @@
 import { css } from '@emotion/react';
 
 export const containerStyling = css({
-  position: 'absolute',
+  position: 'fixed',
   bottom: '24px',
   display: 'flex',
   flexDirection: 'column',
